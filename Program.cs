8d4d784ad9b6869d9fd20f1fb59776aca59e69c9
Michał Wielopolski <77637937+wielopolski@users.noreply.github.com>
2023-05-22 11:04:36 +0200

    add old config

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
 using RestaurMap.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -42,3 +44,57 @@ app.MapControllerRoute(
 app.MapRazorPages();
 
 app.Run();
+
+/*
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using RestaurMap.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//MongoDB
+var pack = new ConventionPack
+{
+	new CamelCaseElementNameConvention(),
+	new StringIdStoredAsObjectIdConvention()
+};
+
+ConventionRegistry.Register("restaurmap_convenction", pack, _ => true);
+MongoUrl url = new MongoUrl(builder.Configuration.GetConnectionString("MongoDB"));
+MongoClientSettings settings = MongoClientSettings.FromUrl(url);
+MongoClient client = new MongoClient(settings);
+IMongoDatabase database = client.GetDatabase(url.DatabaseName);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseMigrationsEndPoint();
+}
+else
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();*/
