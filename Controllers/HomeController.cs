8e916538540bd771688588eb482801a4f935d214
Sopliica <92584815+Sopliica@users.noreply.github.com>
2023-06-19 00:02:07 +0200

    Temporary Changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,25 +9,19 @@ namespace RestaurMap.Controllers;
 public class HomeController : Controller
 {
 	private readonly ILogger<HomeController> _logger;
-	private readonly Scrapper _scrapper;
 
-	public HomeController(ILogger<HomeController> logger, Scrapper scrapper)
+	public HomeController(ILogger<HomeController> logger)
 	{
 		_logger = logger;
-		_scrapper = scrapper;
-		_scrapper.Scrapp();
 	}
-
 	public IActionResult Index()
 	{
 		return View();
 	}
-
 	public IActionResult Privacy()
 	{
 		return View();
 	}
-
 	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 	public IActionResult Error()
 	{
