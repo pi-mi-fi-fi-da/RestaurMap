23a3fa6093732bc7b056293d8d8677ade6fb5f71
Michał Wielopolski <77637937+wielopolski@users.noreply.github.com>
2023-06-19 05:30:12 +0200

    add filter, search, and cord to map

--- wwwroot/js/map.js
+++ wwwroot/js/map.js
@@ -1,9 +1,9 @@
-﻿let x = 50.1119000;
-let y = 18.9842351;
+﻿let x = 49.8230000;
+let y = 19.0500000;
 
 var map = L.map("map", {
     center: [x, y],
-    zoom: 12,
+    zoom: 13,
 });
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -12,19 +12,31 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-var circle = L.circle([x, y], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 900
+var markersLayer = new L.LayerGroup();	//layer contain searched elements
+
+map.addLayer(markersLayer);
+
+var controlSearch = new L.Control.Search({
+	position: 'topright',
+	layer: markersLayer,
+	initial: false,
+	zoom: 12,
+	marker: false
 }).addTo(map);
 
-var polygon = L.polygon([
-    [x, y],
-    [50.1162706, 18.9741339],
-    [50.1164000, 18.9793800]
-]).addTo(map);
+//var circle = L.circle([x, y], {
+//    color: 'red',
+//    fillColor: '#f03',
+//    fillOpacity: 0.5,
+//    radius: 900
+//}).addTo(map);
 
-var marker = L.marker([x, y], ).addTo(map);
-var marker = L.marker([50.1162706, 18.9741339], ).addTo(map);
-var marker = L.marker([50.1164000, 18.9793800], ).addTo(map);
+//var polygon = L.polygon([
+//    [x, y],
+//    [50.1162706, 18.9741339],
+//    [50.1164000, 18.9793800]
+//]).addTo(map);
+
+//var marker = L.marker([x, y], ).addTo(map);
+//var marker = L.marker([50.1162706, 18.9741339], ).addTo(map);
+//var marker = L.marker([50.1164000, 18.9793800], ).addTo(map);
